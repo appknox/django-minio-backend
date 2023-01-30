547284c5973487858931f8886c79e0601f35327a
Subho Halder <subho.halder@gmail.com>
2023-01-31 03:28:01 +0530

    USe the forked version for quicker TAT

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 
 setup(
-    name='django-minio-backend',
+    name='ak-django-minio-backend',
     version=get_git_version(),
     packages=find_packages(),
     include_package_data=True,
